3d069d36656160fa4a813347c47cbae524a47fb5
Daniel Souza <workdndsza@gmail.com>
2023-02-15 19:14:46 -0300

    finisnh

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -10,3 +10,77 @@ type Book = {
     isFiction: boolean;
     datePublished: Date;
 };
+
+
+async function seed() {
+    await Promise.all(
+        getAuthors().map((author) => {
+            return db.author.create({
+                data: {
+                    firstName: author.firstName,
+                    lastName: author.lastName
+                }
+            })
+        })
+    )
+    const author = await db.author.findFirst({
+        where: {
+            firstName: "Steve",
+            lastName: "Chibbs"
+        }
+    })
+
+    await Promise.all(
+        getBooks().map((book) => {
+            const { title, isFiction, datePublished } = book
+
+            return db.book.create({
+                data: {
+                    title,
+                    isFiction,
+                    datePublished,
+                    authorId: author.id
+                }
+            })
+        })
+    )
+}
+
+seed()
+
+function getAuthors(): Array<Author> {
+    return [
+        {
+            firstName: "John",
+            lastName: "Doe",
+        },
+        {
+            firstName: "Willian",
+            lastName: "Mickbird",
+        },
+        {
+            firstName: "Steve",
+            lastName: "Chibbs",
+        },
+    ]
+}
+
+function getBooks(): Array<Book> {
+    return [
+        {
+            title: "Sapiens",
+            isFiction: false,
+            datePublished: new Date(),
+        },
+        {
+            title: "Homo Deus",
+            isFiction: false,
+            datePublished: new Date(),
+        },
+        {
+            title: "Fook Hero",
+            isFiction: true,
+            datePublished: new Date(),
+        },
+    ]
+}
