ad02a58677b941f42cec8675d438cc1ab4e3995c
Daniel Souza <workdndsza@gmail.com>
2023-02-15 18:47:46 -0300

    creating the models and the first types

---
+++ prisma/seed.ts
@@ -0,0 +1,12 @@
+import { db } from "../src/utils/db.server";
+
+type Author = {
+    firstName: string;
+    lastName: string;
+};
+
+type Book = {
+    title: string;
+    isFiction: boolean;
+    datePublished: Date;
+};
