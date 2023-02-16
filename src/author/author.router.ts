7b2c5de23957dd724deabbc36bb7bda564917589
Daniel Souza <workdndsza@gmail.com>
2023-02-16 13:11:14 -0300

    creating the post for add new authors

--- src/author/author.router.ts
+++ src/author/author.router.ts
@@ -29,3 +29,18 @@ authorRouter.get("/:id", async (request: Request, response: Response) => {
         return response.status(500).json(error.message)
     }
 })
+
+// POST: Create a author
+authorRouter.post("/", body("firstName").isString(), body("lastName").isString(), async (request: Request, response: Response) => {
+    const errors = validationResult(request)
+    if (!errors.isEmpty()) {
+        return response.status(400).json({ errors: errors.array() })
+    }
+    try {
+        const author = request.body
+        const newAuthor = await AuthorService.createAuthor(author)
+        return response.status(201).json(newAuthor)
+    } catch (error: any) {
+        return response.status(500).json(error.message)
+    }
+})
