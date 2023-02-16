c761b49656f1936b6d3407965f537c2273d8c531
Daniel Souza <workdndsza@gmail.com>
2023-02-16 14:35:57 -0300

    finishing the api

--- src/book/book.router.ts
+++ src/book/book.router.ts
@@ -26,3 +26,45 @@ bookRouter.get("/:id", async (request: Request, response: Response) => {
         return response.status(500).json(error.message)
     }
 })
+
+// POST: Add a book
+bookRouter.post("/", body("title").isString(), body("authorId").isInt(), body("datePublished").isDate(),body("isFiction").isBoolean() ,async (request: Request, response: Response) => {
+    const errors = validationResult(request)
+    if (!errors.isEmpty()) {
+        return response.status(400).json({ errors: errors.array() })
+    }
+    try {
+        const book = request.body
+        const newBook = await BookService.createBook(book)
+        return response.status(201).json(newBook)
+    } catch (error: any) {
+        return response.status(500).json(error.message)
+    }
+})
+
+// PUT: Update a book
+bookRouter.put("/:id", body("title").isString(), body("authorId").isInt(), body("datePublished").isDate().toDate(),body("isFiction").isBoolean() ,async (request: Request, response: Response) => {
+    const errors = validationResult(request)
+    if (!errors.isEmpty()) {
+        return response.status(400).json({ errors: errors.array() })
+    }
+    try {
+        const id: number = parseInt(request.params.id, 10)
+        const book = request.body
+        const newBook = await BookService.updateBook(book, id)
+        return response.status(201).json(newBook)
+    } catch (error: any) {
+        return response.status(500).json(error.message)
+    }
+})
+
+// DELETE: Delete a Book
+bookRouter.delete("/:id", async (request: Request, response: Response) => {
+    const id: number = parseInt(request.params.id, 10)
+    try {
+        await BookService.deleteBook(id)
+        return response.status(204).json("The book has been successfully deleted")
+    } catch (error: any) {
+        return response.status(500).json(error.message)
+    }
+})
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -10,6 +10,13 @@ type BookRead = {
     // authorId: number
 }
 
+type BookWrite = {
+    title: string,
+    isFiction: boolean,
+    datePublished: Date
+    authorId: number
+}
+
 export const listBooks = async(): Promise<BookRead[]> => {
     return db.book.findMany({
         select: {
@@ -46,6 +53,71 @@ export const getBook = async(id: number): Promise<BookRead | null> => {
                     lastName: true
                 }
             }
+        }
+    })
+}
+
+export const createBook = async(book: BookWrite): Promise<BookRead> => {
+    const { title, authorId, datePublished, isFiction } = book
+    const parsedDate: Date = new Date(datePublished)
+
+    return db.book.create({
+        data: {
+            title,
+            authorId,
+            isFiction,
+            datePublished: parsedDate
+        },
+        select: {
+            id: true,
+            title: true,
+            datePublished: true,
+            isFiction: true,
+            author: {
+                select: {
+                    id: true,
+                    firstName: true,
+                    lastName: true
+                }
+            }
+            // authorId: true,
+        },
+    })
+}
+
+export const updateBook = async (book: BookWrite, id: number): Promise<BookRead> => {
+    const { title, authorId, datePublished, isFiction } = book
+    return db.book.update({
+        where: {
+            id,
+        },
+        data: {
+            title,
+            isFiction,
+            datePublished,
+            authorId
+        },
+        select: {
+            id: true,
+            title: true,
+            datePublished: true,
+            isFiction: true,
+            author: {
+                select: {
+                    id: true,
+                    firstName: true,
+                    lastName: true
+                }
+            }
+            // authorId: true,
+        },
+    })
+}
+
+export const deleteBook = async (id: number): Promise<void> => {
+    await db.book.delete({
+        where: {
+            id,
         }
     })
 }
